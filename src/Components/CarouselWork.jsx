d8cc6421f9e029d45c5959d10f6e15b0630b931e
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-10-25 16:21:59 -0300

    work carousel

---
+++ src/Components/CarouselWork.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import "../Styles/Carousel.css";
+
+const CarouselWork = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [currentImageIndex, images.length]);
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+  };
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex(
+      (currentImageIndex - 1 + images.length) % images.length
+    );
+  };
+  return (
+    <div className="carousel-container">
+      <button onClick={handlePrevImage} className="carousel-button">
+        {"<"}
+      </button>
+      {ImagesArray.AkaiTecho.map((image, index) => (
+        <div key={index}>
+          <img
+            src={image}
+            alt={`Image ${index + 1}`}
+            className="carousel-image"
+          />
+        </div>
+      ))}
+      <button onClick={handleNextImage} className="carousel-button">
+        {">"}
+      </button>
+    </div>
+  );
+};
+
+export default CarouselWork;
--- src/assets/trabajos/images.js
+++
@@ -1,5 +0,0 @@
-export const AkaiTecho = [
-    { src: require("../trabajos/AkaiTecho2.jpg") },
-    { src: require("../trabajos/AkaiTecho3.jpg") },
-    { src: require("../trabajos/AkaiTecho4.jpg") },
-  ];
